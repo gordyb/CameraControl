394a291653f41dcedf7f461364f9ce16b8937416
Gordy Bone <gordybone@gmail.com>
2012-06-27 15:04:52 +0100

    Added all required functionality

--- src/com/develogical/camera/Camera.java
+++ src/com/develogical/camera/Camera.java
@@ -1,24 +1,54 @@
 package com.develogical.camera;
 
-public class Camera {
+public class Camera implements WriteListener {
 
+    CameraStatus status = CameraStatus.OFF;
+
+    private enum CameraStatus {
+        ON, OFF
+    }
+
+    private boolean writing = false;
+    private boolean offPending = false;
 
     private Sensor sensor;
+    private MemoryCard memoryCard;
 
-    public Camera(Sensor sensor) {
+    public Camera(Sensor sensor, MemoryCard memoryCard) {
         this.sensor = sensor;
+        this.memoryCard = memoryCard;
     }
 
     public void pressShutter() {
-        // not implemented
+        if(status == CameraStatus.ON) {
+            writing = true;
+            memoryCard.write(sensor.readData());
+        }
     }
 
     public void powerOn() {
         sensor.powerUp();
+        status = CameraStatus.ON;
     }
 
     public void powerOff() {
-        sensor.powerDown();
+        if(!writing) {
+            sensor.powerDown();
+            status = CameraStatus.OFF;
+        } else {
+            offPending = true;
+        }
     }
+
+    @Override
+    public void writeComplete() {
+        writing = false;
+        if(offPending) {
+            powerOff();
+            offPending = false;
+        }
+    }
+
+
 }
 
